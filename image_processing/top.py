f500144bbf3cfe9380259ba41b7d6b514f5b6ee9
DendriveVlad <dendrivevlad@gmail.com>
2022-04-24 15:16:44 +0500

    optimization and -admins

--- image_processing/top.py
+++ image_processing/top.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 from requests import get
 
-from config import LEVEL_POINTS
+from config import LEVEL_POINTS, ROLES
 
 
 def get_top(guild, db):
@@ -20,14 +20,16 @@ def get_top(guild, db):
 
     user_level = []
     container = {}
-    for date in db.select("users", f"", "user_id", "points"):
+    for date in db.select("users", "points > 1000", "user_id", "points"):
+        if filter(lambda role: role.id in (ROLES["Admin"], ROLES["Owner"], ROLES["Vlad"]), guild.get_member(date["user_id"]).roles):
+            continue
         container[date["user_id"]] = date["points"]
     for id, points in sorted(container.items(), key=lambda item: item[1], reverse=True):
         user_level.append((id, points))
 
     place = 0
-    for h in range(51, 560):
-        if h > 100 and h % 50 == 1 and place < 10:
+    for h in range(101, 560, 50):
+        if place < 10:
             level = 0
             for lvl, need_points in LEVEL_POINTS.items():
                 if user_level[place][-1] < need_points:
@@ -46,23 +48,29 @@ def get_top(guild, db):
             draw.text((115 + name_font.getsize(str(user.nick if user.nick else user.name))[0], h - 42), str(user_level[place][-1]), font=name_font, fill=(67, 120, 211))  # points
             draw.text((10, h - 35), (" " if place != 9 else "") + str(place + 1) + ")", font=place_font, fill=(67, 120, 211))  # rank
             place += 1
+        else:
+            break
 
     user_gold = []
     container = {}
-    for date in db.select("users", f"", "user_id", "gold"):
+    for date in db.select("users", f"gold != 0", "user_id", "gold"):
+        if filter(lambda role: role.id in (ROLES["Admin"], ROLES["Owner"], ROLES["Vlad"]), guild.get_member(date["user_id"]).roles):
+            continue
         container[date["user_id"]] = date["gold"]
     for id, gold in sorted(container.items(), key=lambda item: item[1], reverse=True):
         user_gold.append((id, gold))
 
     place = 0
-    for h in range(51, 560):
-        if h > 100 and h % 50 == 1 and place < 10:
+    for h in range(101, 560, 50):
+        if place < 10:
             user = guild.get_member(user_gold[place][0])
             top.paste(Image.open(BytesIO(get(str(user.avatar.url), stream=True).content)).convert("RGBA").resize((41, 41), Image.ANTIALIAS), (1510, h - 40))  # картинка
             draw.text((1490 - name_font.getsize(str(user_gold[place][-1]))[0] - name_font.getsize(user.nick if user.nick else user.name)[0], h - 42), str(user_gold[place][-1]), font=name_font, fill=(228, 180, 30), align="right")  # gold
             draw.text(((1599 - name_font.getsize(user.nick if user.nick else user.name)[0]) - 100, h - 42), user.nick if user.nick else user.name, font=name_font, fill=user.color.to_rgb(), align="right")  # name
             draw.text((1560, h - 35), "(" + str(place + 1), font=place_font, fill=(228, 180, 30), align="right")  # rank
             place += 1
+        else:
+            break
 
     top.save("top.png")
     return "top.png"
